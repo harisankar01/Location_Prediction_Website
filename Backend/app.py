59b64c85d4b1025426ac3453a77d35d4944daa05
hari <hariharasankar.k.a@gmail.com>
2022-06-28 14:53:57 +0530

    Backend machine learning model completed

--- Backend/app.py
+++ Backend/app.py
@@ -1,15 +1,54 @@
-from crypt import methods
-from flask import Flask, jsonify
+from tkinter import Image
+from flask import Flask, jsonify, request
+import numpy as np
+from werkzeug.utils import secure_filename
+import io
+import PIL.Image as Image
+import tensorflow as tf
+import tensorflow_hub as hub
 app = Flask(__name__)
 
+tf.compat.v1.disable_eager_execution()
+graph = tf.Graph()
+with tf.compat.v1.Session(graph=graph) as session:
+    model = hub.Module(
+        "https://tfhub.dev/google/on_device_vision/classifier/landmarks_classifier_asia_V1/1")
+IMAGE_SHAPE = (321, 321)
 
-@app.route('/',methods=["GET"])
+
+@app.route('/', methods=["GET"])
 def index():
-    return jsonify("gjkk")
+    return jsonify("JKGF")
 
-@app.route('/predict',methods=["POST"])
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    return jsonify("predicted")
+    image = request.files['file']
+    if not image:
+        return "Image prediction failed"
+    file = secure_filename(image.name)
+    mime_file = image.mimetype
+    if not file or not mime_file:
+        return 'Bad upload!', 400
+    image_bytes = image.read()
+    image = Image.open(io.BytesIO(image_bytes))
+    final_image = Image.new("RGB", IMAGE_SHAPE, (255, 255, 255))
+    final_image.paste(image, None, mask=image.split()[3])
+    final_image = np.asarray(final_image)[None, ...]
+    print(type(final_image))
+    print("The shape of final image is " + str(final_image.shape))
+    # imggg = tf.image.resize(image, (321, 321), ,name=None)
+    # with g.as_default():
+    #     result = model([image], as_dict=True)
+    # g.finalize()
+    # shape = tf.TensorSpec(shape=[None, 321, 321, 3],
+    #                       dtype=tf.float32, name=None)
+    result = model.get_input_info_dict()
+    with tf.compat.v1.Session(graph=graph) as session:
+        resultss = session.run(model(final_image, as_dict=True))
+    print(resultss)
+    return jsonify("upload success")
+
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True) 
+    app.run(host='127.0.0.1', port=5000, debug=True)
--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React,{useState,useRef} from 'react';
 
-import './App.css';
 
 function App() {
+
+ const [Image, setImage] = useState<string>("")
+const [Prediction, setPrediction] = useState("")  
+
+    let formdata:FormData;
+  const CaptureFile=(e: React.ChangeEvent<HTMLInputElement>)=>{
+    if(e.target.files && e.target.files.length>0){
+      const file:File=e.target.files[0]  
+      formdata = new FormData();
+      formdata.append('file', file);
+      formdata.append('Name', "image_tobe_predicted");
+      console.log(formdata);
+    }
+  }
+    const Predict=async()=>{
+      const prediction =await fetch('/predict',{
+      method:"POST",
+      body: formdata
+      });
+      const result=await prediction.json()
+      setPrediction(result);
+      console.log(result);
+      
+    }
   return (
     <div className="Starter">
-    <h1>Lets do this</h1>
+    <input type='file' accept="image/*" name='loc' onChange={CaptureFile}/>
+    <button type='button' onClick={Predict}></button>
     </div>
   );
 }
